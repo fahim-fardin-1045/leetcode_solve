e2531167579c446d4cf3c7935aad24e5c9c3c69d
fahim-fardin-1045 <fahimfardin782@gmail.com>
2026-02-09 23:00:02 +0600

    Palindrome_Number

---
+++ golang/solutions/Palindrome Number/Palindrome_Number.go
@@ -0,0 +1,18 @@
+package main
+
+import "fmt"
+
+func isPalindrome(x int) bool {
+	if x < 0 || (x > 0 && x%10 == 0) {
+		return false
+	}
+	y := 0
+	for ; y < x; x /= 10 {
+		y = y*10 + x%10
+	}
+	return x == y || x == y/10
+}
+
+func main() {
+	fmt.Println(isPalindrome(121)) // true
+}
